e5271eec73ad60f9a002826c556c7047c8ed3753
Jonathan Arellano <jonathan.arellano.216@gmail.com>
2016-10-31 19:00:04 -0700

    added basic models without associations

---
+++ api/models/Message.js
@@ -0,0 +1,30 @@
+/**
+ * Message.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    name : { type: 'string' },
+
+    username : {
+      type: 'string',
+      unique: true
+    },
+
+    phone : { type: 'string' },
+
+    email : { type: 'string' },
+
+    service: { type: 'string'},
+
+    subject : { type: 'string' },
+
+    message : { type: 'longtext' }
+  }
+};
+
